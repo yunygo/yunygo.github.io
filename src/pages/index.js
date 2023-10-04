e2b7a13398c53ef5d7a4c87545c8a0673673a202
Young <yunyg@qq.com>
2023-10-04 14:58:21 +0800

    title fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="Young的博客" description="Young的博客">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
